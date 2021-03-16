28094eb602df195b35c707f7fe14726814e92988
andersonms92 <anderson.matos.santos@everis.com>
2021-03-16 14:10:40 -0300

    listOf

---
+++ Collections/src/main/kotlin/TesteList.kt
@@ -0,0 +1,24 @@
+package com.example.collectionsarrayslists
+
+fun main() {
+    val joao = Funcionario("João", 1000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{ println(it) }
+    println("-----------------------------------")
+    println(funcionarios.find{it.nome == "Maria"})
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+){
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salario: $salario
+        """.trimIndent()
+}
