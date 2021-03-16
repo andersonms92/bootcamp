0720ae065eefd200cd1988b1f640fa35bb95fb98
andersonms92 <anderson.matos.santos@everis.com>
2021-03-16 14:38:44 -0300

    sortedBy e groupBy

--- Collections/src/main/kotlin/TesteList.kt
+++ Collections/src/main/kotlin/TesteList.kt
@@ -1,20 +1,29 @@
 package com.example.collectionsarrayslists
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
     funcionarios.forEach{ println(it) }
     println("-----------------------------------")
     println(funcionarios.find{it.nome == "Maria"})
+
+    println("-----------------------------------")
+    funcionarios.sortedBy{ it.salario}.forEach{ println(it) }
+
+    println("-----------------------------------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach{println(it)}
+
+    
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
