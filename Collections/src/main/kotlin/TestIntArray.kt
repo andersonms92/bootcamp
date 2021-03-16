baf0762d9419ea65b743ac5b35380ca7466c8e70
andersonms92 <anderson.matos.santos@everis.com>
2021-03-16 11:05:04 -0300

    Collections

---
+++ Collections/src/main/kotlin/TestIntArray.kt
@@ -0,0 +1,15 @@
+package com.example.collectionsarrayslists
+
+fun main(){
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values){
+        println(valor)
+    }
+}
