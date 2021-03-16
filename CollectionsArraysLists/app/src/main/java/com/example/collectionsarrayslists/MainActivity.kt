21ae7b40be5128cc0b4368af458191002083d9df
andersonms92 <anderson.matos.santos@everis.com>
2021-03-16 10:23:08 -0300

    CollectionsArrayLists

---
+++ CollectionsArraysLists/app/src/main/java/com/example/collectionsarrayslists/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.collectionsarrayslists
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.collectionsarrayslists.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
