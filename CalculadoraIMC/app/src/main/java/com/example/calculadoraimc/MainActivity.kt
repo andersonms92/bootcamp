cb498c99d6c5affc88a3a55c08e6b2a3cbdbba60
andersonms92 <anderson.matos.santos@everis.com>
2021-03-18 19:51:16 -0300

    criação relative, linear e constraint layout

--- CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.calculadoraimc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged{text, _, _, _ ->
+            textView.setBackgroundColor(Color.GREEN)
+        }
+        calcularBTN?.setOnClickListener{
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            textView2.text = "Seu IMC é igual a : %.2f".format(imc)
+        }
     }
 }
