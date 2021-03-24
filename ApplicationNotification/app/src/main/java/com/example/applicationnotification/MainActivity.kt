c31316f939f1f7580dad85bc832577b3e894a8a8
andersonms92 <anderson.matos.santos@everis.com>
2021-03-24 10:52:54 -0300

    Criando psu notificação

---
+++ ApplicationNotification/app/src/main/java/com/example/applicationnotification/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.applicationnotification
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import com.google.firebase.iid.FirebaseInstanceId
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener{
+            this.showNotification("1234", "bootcamp Android", "Kotlin Android curso")
+        }
+
+        Log.i("**newToken", FirebaseInstanceId.getInstance().token.toString())
+    }
+}
---
+++ ApplicationNotification/app/src/main/java/com/example/applicationnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,25 @@
+package com.example.applicationnotification
+
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val tag = "FirebaseMessagingServ"
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.i(tag, remoteMessage.from.toString())
+
+        if (remoteMessage.notification != null) {
+            this.showNotification("1234",
+            remoteMessage.notification?.title.toString(),
+            remoteMessage.notification?.body.toString())
+        }
+    }
+}
